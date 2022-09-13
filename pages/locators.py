e4629e701a35d4bb34dca04141c90e400b40622a
unknown <talurm@mail.ru>
2022-09-13 22:42:28 +0300

    Tests were groupped, setup fixture added

--- pages/locators.py
+++ pages/locators.py
@@ -1,12 +1,17 @@
 from selenium.webdriver.common.by import By
 
 class BasePageLocators():
+    BASKET_ISEMPTY = (By.CSS_SELECTOR, "#content_inner>p")
+    BASKET_PAGE_LINK = (By.CSS_SELECTOR, "span a.btn.btn-default")
+    CURRENT_LANGUAGE = (By.CSS_SELECTOR, '[selected="selected"]')
+    ITEM_LOCATOR = (By.CSS_SELECTOR, ".basket-items")
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-    BASKET_PAGE_LINK = (By.CSS_SELECTOR, "span a.btn.btn-default")
-    ITEM_LOCATOR = (By.CSS_SELECTOR, ".basket-items")
-    BASKET_ISEMPTY = (By.CSS_SELECTOR, "#content_inner>p")
-    CURRENT_LANGUAGE = (By.CSS_SELECTOR, '[selected="selected"]')
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, '[name = "registration-email"]')
+    REGISTRATION_PASSWORD1 = (By.CSS_SELECTOR, '[name = "registration-password1"]')
+    REGISTRATION_PASSWORD2 = (By.CSS_SELECTOR, '[name = "registration-password2"]')
+    REGISTRATION_SUBMIT = (By.CSS_SELECTOR, '[name = "registration_submit"]')
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
@@ -17,10 +22,12 @@ class MainPageLocators():
 
 class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BASKET_PRICE = (By.CSS_SELECTOR, ".basket-mini")
+    BOOK_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alertinner")
     TITLE_LOCATOR = (By.CSS_SELECTOR, ".col-sm-6.product_main>h1")
-    BOOK_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
-    BASKET_PRICE = (By.CSS_SELECTOR, ".basket-mini")
+    
+    
 
     
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,38 @@
 from .pages.product_page import ProductPage
 from .pages.base_page import BasePage
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 import time
 import pytest
+import faker
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        self.page = LoginPage(browser, link)
+        self.page.open()
+        f = faker.Faker()
+        email = f.email()
+        password = str(time.time()) + "??!!"
+        self.page.register_new_user(email=email, password=password)
+        self.page.should_be_authorized_user()
+        
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link, 0)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_success_message()
+
+    
 
 @pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -62,6 +92,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.skip
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
